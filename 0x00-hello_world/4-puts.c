519653dd37dad8005808154255840b9e38357d4e
AerinLom <aerinlom1@gmail.com>
2023-10-05 01:59:30 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)/**
-		*main - entry point
-		*Return: 0 if successful
+		* main - main entry point
+		* Return: 0 always
 		**/
 {
 	puts("\"Programming is like building a multilingual puzzle");
