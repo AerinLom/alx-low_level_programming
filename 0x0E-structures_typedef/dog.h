48e1aa888383b78cf2a57e10cdb86d30cedfca18
AerinLom <aerinlom1@gmail.com>
2023-11-06 09:31:58 -0800

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  *struct dog - structure of dog
+  *@name: name of dog
+  *@age: age of dog
+  *@owner: owner name of dog
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
