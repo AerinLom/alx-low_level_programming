6b5e66095536cdf02b168f5cb0826169de6c26de
AerinLom <aerinlom1@gmail.com>
2023-11-09 05:17:46 -0800

    One woman's constant is another woman's variable

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,6 +7,8 @@
   *@separator: separator between strings
   *@n: number of strings
   *@...:strings to be printed
+  *Description: If separator is NULL, it is not printed
+  *	If one of the strings if NULL, (nil) is printed instead.
   */
 
 void print_strings(const char *separator, const unsigned int n, ...)
