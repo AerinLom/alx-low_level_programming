a15b2b1d1db102e7b3972677c4962229384eb9bd
AerinLom <aerinlom1@gmail.com>
2023-11-16 10:57:59 -0800

    Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+  *list_len - function that counts elements in a list
+  *@h: pointer to the list
+  *Return: count of elements in the list
+  */
+
+size_t list_len(const list_t *h)
+{
+	size_t element_count;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		element_count++;
+	}
+	return (element_count);
+}
