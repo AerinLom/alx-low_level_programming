41ee703cc24bb825737b425092bf07511f1c9f95
AerinLom <aerinlom1@gmail.com>
2023-11-16 10:42:49 -0800

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+  *print_list - fuction that prints all elements of a list
+  *@h: pointer to a list
+  *Return: amount of nodes in h
+  */
+
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while(h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		node_count++;
+		h = h->next;
+	}
+	return (node_count);
+}
+
