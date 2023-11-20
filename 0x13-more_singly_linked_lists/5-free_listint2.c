595c877e790f45bde1b152f7e1012c8da45233da
AerinLom <aerinlom1@gmail.com>
2023-11-20 09:09:01 -0800

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+  *free_listint2 - function that frees list
+  *@head: beginning of list
+  */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *binned_list, *list_iter;
+
+	binned_list = *head;
+
+	while (binned_list != NULL)
+	{
+		list_iter = binned_list->next;
+		free(binned_list);
+		binned_list = list_iter;
+	}
+	*head = NULL;
+}
