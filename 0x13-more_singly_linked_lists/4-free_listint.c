209fd6967178db3b7a2cee35d044ac44092d338d
AerinLom <aerinlom1@gmail.com>
2023-11-20 08:54:33 -0800

    Task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  *free_listint - function that frees list
+  *@head: beginning of list
+  */
+
+void free_listint(listint_t *head)
+{
+	listint_t *bin_node, *skip_node;
+	
+	bin_node = head;
+
+	while (bin_node != NULL)
+	{
+		skip_node = bin_node->next;
+		free(bin_node);
+		bin_node = skip_node;
+	}
+}
