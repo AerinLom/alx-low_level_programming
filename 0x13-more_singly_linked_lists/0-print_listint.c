bd055a3ef6b0b4ecfe27d0872d16dc7c5cce8a57
AerinLom <aerinlom1@gmail.com>
2023-11-20 07:26:03 -0800

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+  *print_listint - prints all elecments of a list
+  *@h: list to print from
+  *Return: number of nodes
+  */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t node_size = 0;
+
+	while (h != NULL)
+	{
+		if (!h->n)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("%d\n", h->n);
+		}
+		node_size++;
+		h = h->next;
+	}
+	return(node_size);
+}
