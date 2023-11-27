72eb4f48e2993b570c37825d4f3242499dd6bb82
AerinLom <aerinlom1@gmail.com>
2023-11-27 09:51:32 -0800

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,10 +31,10 @@ int create_file(const char *filename, char *text_content)
 	for (num_letters = 0; text_content[num_letters]; num_letters++)
 		;
 	write_file = write(file_descrip, text_content, num_letters);
-	
+
 	if (write_file == -1)
 		return (-1);
-	
+
 	close(file_descrip);
 	return (1);
 }
