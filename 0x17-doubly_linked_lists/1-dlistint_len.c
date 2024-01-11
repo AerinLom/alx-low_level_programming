52d6f1af5f90cbce60385100f55040b3fb2d5824
AerinLom <aerinlom1@gmail.com>
2024-01-11 05:15:38 -0800

    Task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,12 +8,12 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-        size_t num_elements = 0;
+	size_t num_elements = 0;
 
-        while (h != NULL)
-        {
-                h = h->next;
-                num_elements++;
-        }
-        return (num_elements);
+	while (h != NULL)
+	{
+		h = h->next;
+		num_elements++;
+	}
+	return (num_elements);
 }
