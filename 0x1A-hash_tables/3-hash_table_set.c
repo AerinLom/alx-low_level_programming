08d54c2f5ac9cb5dcdd6e996c1598c93be039533
AerinLom <aerinlom1@gmail.com>
2024-02-01 10:51:41 -0800

    Task 3, more adjustments

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,11 +12,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_element = NULL, *swap = NULL;
 	unsigned long int position;
+	char *copied_data = strdup(value);
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 	{
 		return (0);
 	}
+	if (copied_data == NULL)
+	{
+		return (0);
+	}
+
 	position = key_index((const unsigned char *)key, ht->size);
 	swap = ht->array[position];
 
@@ -25,11 +31,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(swap->key, key) == 0)
 		{
 			free(swap->value);
-			swap->value = strdup(value);
-			if (swap->value == NULL)
-			{
-				return (0);
-			}
+			swap->value = copied_data;
 			return (1);
 		}
 		swap = swap->next;
@@ -37,16 +39,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_element = malloc(sizeof(hash_node_t));
 	if (new_element == NULL)
 	{
-		free(new_element);
+		free(copied_data);
 		return (0);
 	}
-	new_element->value = strdup(value);
-	if (new_element->value == NULL)
+	new_element->key = strdup(key);
+	if (new_element->key == NULL)
 	{
-		free(new_element->key);
 		free(new_element);
+		free(copied_data);
 		return (0);
 	}
+	new_element->value = copied_data;
 	new_element->next = ht->array[position];
 	ht->array[position] = new_element;
 	return (1);
