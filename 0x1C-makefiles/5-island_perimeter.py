1bc3191a63384d0d4b59c2eef894669cdfdd3b5f
AerinLom <aerinlom1@gmail.com>
2024-03-20 06:38:36 -0700

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,6 +3,7 @@
 Defines a function that calculates perimeter
 """
 
+
 def island_perimeter(grid):
     """
     Returns the perimeter of an 'island'
@@ -13,7 +14,7 @@ def island_perimeter(grid):
     num_edges = sum(grid[i][j] == 1 and (j > 0 and grid[i][j - 1] == 1)
                     for i in range(latitude) for j in range(longitude))
     num_edges += sum(grid[i][j] == 1 and (i > 0 and grid[i - 1][j] == 1)
-                      for i in range(latitude) for j in range(longitude))
+                     for i in range(latitude) for j in range(longitude))
 
     num_cells = sum(row.count(1) for row in grid)
 
